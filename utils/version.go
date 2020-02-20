33d8e444a011e4d27cc39f4fcb8b16e48667c432
QRaimbault <quentin.raimbault@gmail.com>
2020-02-21 00:13:48 +0100

    Build: Remove release drafter
    Fix: Code style
    
    Fix: Code style
    
    Fix: Code style
    
    Fix: Code style

--- utils/version.go
+++ utils/version.go
@@ -1,9 +1,14 @@
 package utils
 
 var (
+	// MajorVersion is the hardcoded major version of Cerberus.
 	MajorVersion = 0
+	// MinorVersion is the hardcoded minor version of Cerberus.
 	MinorVersion = 12
+	// PatchVersion is the hardcoded patch version of Cerberus.
 	PatchVersion = 1
+	// VersionHash is the commit hash for the built version injected by the linker.
 	VersionHash = ""
+	// BuildTime is the build time in RFC3309 format injected by the linker.
 	BuildTime = ""
 )
